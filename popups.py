52c397d2ec9d50428d15192ed8b5e3a8bb1ff852
Tyler Spadgenske <spadgens@lakedalelink.net>
2014-01-14 16:46:46 -0600

    game over popup added to class

--- popups.py
+++ popups.py
@@ -9,9 +9,8 @@ from button import Button
 pygame.init()
 
 class Popups(object, Button):
-    def __init__(self):
-        self.windowSurface = pygame.display.set_mode((300, 350), 0, 32)
-        pygame.display.set_caption('GAME OVER')
+    def __init__(self, window):
+        self.window = window
         self.mainClock = pygame.time.Clock()
 
         self.WHITE = (255, 255, 255)
@@ -20,16 +19,18 @@ class Popups(object, Button):
         self.font = pygame.font.SysFont(None, 48)
         self.text = self.font.render('GAME OVER', True, self.ORANGE, self.WHITE)
         self.textRect = self.text.get_rect()
-        self.textRect.centerx = self.windowSurface.get_rect().centerx
-        self.textRect.centery = self.windowSurface.get_rect().centery - 150
-        self.location = [200, 300]
+        self.textRect.centerx = self.window.get_rect().centerx
+        self.textRect.centery = self.window.get_rect().centery - 150
+        self.location = [550, 365]
         self.enlarge_reload = False
         self.reload = False
-        self.backLocation = [100, 300]
+        self.backLocation = [660, 365]
         self.enlarge_back = False
         self.enlarge_sound = False
         self.enlarge_pause = False
         self.back = False
+
+        self.buttons = Button(self.window)
         
     def alert(self):
         pass
@@ -37,33 +38,33 @@ class Popups(object, Button):
     def endgame(self):
         while True:
             self.tacoImage = pygame.image.load('Characters\\blownUp.gif')
-
+            block = {'rect':pygame.Rect(475, 115, 250, 300),'color':self.WHITE}
+            pygame.draw.rect(self.window, block['color'], block['rect'])
             #Initialize reload image rect
             self.tacoRect = self.tacoImage.get_rect()
-            self.tacoRect.centerx = 190
-            self.tacoRect.centery = 185
+            self.tacoRect.centerx = self.window.get_rect().centerx + 50
+            self.tacoRect.centery = self.window.get_rect().centery
             self.tacoImageBIG = pygame.transform.scale(self.tacoImage, (150, 150))
 
-            self.windowSurface.blit(self.tacoImageBIG, self.tacoRect)
-            self.windowSurface.blit(self.text, self.textRect)
+            self.reload = self.buttons.roundButton( None, 'reload', self.location)
+            self.back = self.buttons.roundButton( None, 'back', self.backLocation)
+                
+            self.window.blit(self.tacoImageBIG, self.tacoRect)
+            self.window.blit(self.text, self.textRect)
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    self.back = True
+                self.reload = self.buttons.roundButton( event, 'reload', self.location)
+                self.back = self.buttons.roundButton( event, 'back', self.backLocation)
+
             pygame.display.update()
             self.mainClock.tick()
 
-            self.windowSurface.fill(self.WHITE)
-            self.reload = Button.roundButton(self, event, 'reload', self.location)
-            self.back = Button.roundButton(self, event, 'back', self.backLocation)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.back = True
+                    
             if self.reload or self.back:
                 break
-            
-        pygame.quit()   
+    
         return self.reload, self.back
-        
-test = Popups()
-test.endgame()
-
-
-  
